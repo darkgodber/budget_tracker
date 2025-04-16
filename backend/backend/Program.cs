92ecedd78facac4bd3dfc64cf9da1107799a5d22
darkgodber <102028929+darkgodber@users.noreply.github.com>
2025-04-16 17:55:48 +0500

    1.0.2
    - Реализовал работу с Докер (не знаю можно ли это так назвать, но через Docker Desktop все запускается одной кнопкой, как я и хотел).

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -21,10 +21,17 @@ builder.Services.AddSwaggerGen();
 // ...
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BudgetTrackerContext>();
+    //db.Database.Migrate();
+}
+
+
 // Использование CORS (до использования эндпоинтов)
 app.UseCors("AllowReactApp");
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
